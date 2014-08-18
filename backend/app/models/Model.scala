c6f24c2797f8b44a51afdacc8b359a636dd157a2
viossalm <ville.o.salmela@student.jyu.fi>
2014-08-18 21:57:58 +0300

    added comments, and created sample insert into db method

--- backend/app/models/Model.scala
+++ backend/app/models/Model.scala
@@ -3,8 +3,11 @@ import anorm._
 import play.api.db.DB
 import play.api.Play.current
 
-abstract class Model {
+trait Model {
 
+  /*
+   * returns model by id
+   */
   def getById(id: Int, table: String) {
     
     val sqlQuery = SQL(
@@ -19,8 +22,15 @@ abstract class Model {
     }
   }
 
+  /*
+   * Insert a model into db and return an id value if it was successful
+   * Needs to be implemented in every model
+   */
   def insertIntoDB(model: Model)
 
+  /*
+   * Deletes object from db and returns amount of deleted lines
+   */
   def deleteFromDB(id: Int, table: String) {
     DB.withConnection { implicit c =>
       val result: Int = SQL(f"delete from $table%s where id = $id%d").executeUpdate()
--- backend/app/models/Restaurant.scala
+++ backend/app/models/Restaurant.scala
@@ -1,4 +1,19 @@
 package models
+import anorm._
+import play.api.db.DB
+import play.api.Play.current
 
-case class Restaurant(name: String, address: String, city: String, 
-    postalCode: Int, openingHours: String) extends Model
+case class Restaurant(name: String, address: String, city: String,
+  postalCode: Int, openingHours: String) extends Model {
+
+  /*
+   * Insert restaurant into database, returns id.
+   */
+  def insertIntoDB(restaurant: Restaurant) {
+    DB.withConnection { implicit c =>
+      val id: Option[Long] = SQL("insert into Restaurant(name, address, city, postalCode, openingHours) values ({name},{address},{city},{postalCode},{openingHours}")
+        .on('name -> restaurant.name, 'address -> restaurant.address, 'city -> restaurant.city, 'postalCode -> restaurant.postalCode, 'openingHours -> restaurant.openingHours).executeInsert()
+    }
+  }
+
+}
